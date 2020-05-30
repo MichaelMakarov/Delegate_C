97e5b238eccf21432ad3a82752517f224b309794
makarovm <makarovm@is35.ru>
2020-05-30 23:18:21 +0300

    Готовая версия.

---
+++ Delegate.h
@@ -0,0 +1,107 @@
+﻿#pragma once
+
+template<typename>
+class Delegate;
+
+template<class ReturnType, class ... ArgsType>
+class Delegate<ReturnType(ArgsType ...)>
+{
+private:
+	// базовый класс для вызова функции исполнения
+	class BaseInvoker
+	{
+	public:
+		BaseInvoker() {}
+		~BaseInvoker() {}
+		virtual ReturnType Invoke(ArgsType ... args) = 0;
+		virtual BaseInvoker* Clone() = 0;
+	};
+	// класс для вызова обычных функций и лямбда-выражений
+	class FreeInvoker : public BaseInvoker
+	{
+	private:
+		ReturnType(*Function)(ArgsType ...);
+	public:
+		FreeInvoker(ReturnType(*func)(ArgsType ...)) : Function(func) {}
+		~FreeInvoker() {}
+		ReturnType Invoke(ArgsType ... args) { return Function(args ...); }
+		BaseInvoker* Clone() { return static_cast<BaseInvoker*>(new FreeInvoker(Function)); }
+	};
+	// класс для вызова методов классов
+	template <class ClassType>
+	class MembInvoker : public BaseInvoker
+	{
+	private:
+		ReturnType(ClassType::* Function)(ArgsType ...);
+		ClassType* Class;
+	public:
+		MembInvoker(ReturnType(ClassType::* func)(ArgsType ...), ClassType* obj) : Function(func), Class(obj) {}
+		~MembInvoker() {}
+		ReturnType Invoke(ArgsType ... args) { return (Class->*Function)(args ...); }
+		BaseInvoker* Clone() { return static_cast<BaseInvoker*>(new MembInvoker(Function, Class)); }
+	};
+	// класс для вызова конст. методов класса
+	template <class ClassType>
+	class CmemInvoker : public BaseInvoker
+	{
+		ReturnType(ClassType::* Function)(ArgsType ...) const;
+		const ClassType* const Class;
+	public:
+		CmemInvoker(ReturnType(ClassType::* func)(ArgsType ...) const, const ClassType* const obj) : Function(func), Class(obj) {}
+		~CmemInvoker() {}
+		ReturnType Invoke(ArgsType ... args) { return (Class->*Function)(args ...); }
+		BaseInvoker* Clone() { return static_cast<BaseInvoker*>(new CmemInvoker(Function, Class)); }
+	};
+	// вызов перегруженного оператора класса
+	template <class ClassType>
+	class ClssInvoker : public BaseInvoker
+	{
+		ClassType Class;
+	public:
+		ClssInvoker(const ClassType& obj) : Class(obj) {}
+		~ClssInvoker() {}
+		ReturnType Invoke(ArgsType ... args) { return Class(args ...); }
+		BaseInvoker* Clone() { return static_cast<BaseInvoker*>(new ClssInvoker(Class)); }
+	};
+
+	BaseInvoker* pInvoker;
+	// очищение делегата
+	void Clear() {
+		if (pInvoker)
+		{
+			delete pInvoker;
+			pInvoker = nullptr;
+		}
+	}
+public:
+	//конструктор по-умолчанию
+	Delegate() : pInvoker(nullptr) {}
+	// деструктор
+	~Delegate() { Clear(); }
+	// кнструктор для обычной функции
+	Delegate(ReturnType(*func)(ArgsType ...)) : pInvoker(new FreeInvoker(func)) {}
+	// конструктор для метода класса
+	template <class ClassType>
+	Delegate(ReturnType(ClassType::* func)(ArgsType ...), ClassType* obj) : pInvoker(new MembInvoker<ClassType>(func, obj)) {}
+	// конструктор для конст. метода класса
+	template <class ClassType>
+	Delegate(ReturnType(ClassType::* func)(ArgsType ...) const, const ClassType* const obj) : pInvoker(new CmemInvoker<ClassType>(func, obj)) {}
+	// конструктор для класса с перегруженным оператором ()
+	template <class ClassType>
+	Delegate(const ClassType& obj) : pInvoker(new ClssInvoker<ClassType>(obj)) {}
+	// конструктор копирования
+	Delegate(const Delegate& other) : pInvoker(other.pInvoker->Clone()) {}
+	// перегруженный оператор вызова функции с аргументами args
+	ReturnType operator () (ArgsType ... args) { return pInvoker->Invoke(args ...); }
+	// оператор присваивания
+	void operator = (const Delegate& other) { pInvoker = other.pInvoker->Clone(); }
+	// присваивание нового указателя на функцию
+	void operator = (ReturnType(*func)(ArgsType ...)) {
+		Clear(); pInvoker = new FreeInvoker(func);
+	}
+	// присваивание классас перегруженным оператором ()
+	template <class ClassType>
+	void operator = (const ClassType& obj) {
+		Clear(); pInvoker = new ClssInvoker<ClassType>(obj);
+	}
+};
---
+++ main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include "Delegate.h"
+#include <functional>
+
+int Rep(const int& n) { return n; }
+
+class Func
+{
+public:
+	Func() {};
+	int operator () (const int i) { return i * 10; }
+	int Mult(const int i) const { return i * 100; }
+	int Mult(const int i) { return i * 1000; }
+	int Deci(const int i) const { return i * 10; }
+	static int Sqr(const int i) { return i * i; }
+};
+
+int main()
+{
+	// тестирование с использованием йункции Rep и класса Func
+	auto L = [](const char* srr) -> size_t { return 3; };
+	Func func;
+	std::hash<const char*> hasher;
+	std::function<size_t(const char*)> fn = hasher;
+	int (Func:: * fp)(const int) const;
+	fp = &Func::Mult;
+
+	std::cout << "...Delegate testing...\n";
+
+	Delegate<size_t(const char*)> var1(L);
+	std::cout << "Delegate for free function (lambda): " << var1("first") << std::endl;
+	var1 = hasher;
+	std::cout << "Delegate for class with overloaded operator(): " << var1("first") << std::endl;
+	std::cout << "std::function for std::hash: " << fn("qwerty") << std::endl;
+	var1 = fn;
+	std::cout << "Delegate for std::function with std::hash: " << var1("qwerty") << std::endl;
+
+	Delegate<int(const int)> var2(fp, &func);
+	std::cout << "Delegate for const member function: " << var2(3) << std::endl;
+	var2 = &Rep;
+	std::cout << "Delegate for free function: " << var2(-8999) << std::endl;
+
+	Delegate<int(const int)> var3(&Func::Deci, &func);
+	std::cout << "Delegate for const member function: " << var3(55) << std::endl;
+	auto var4 = var3;
+	std::cout << "Delegate copied from previous: " << var4(55) << std::endl;
+
+	Delegate<int(const int)> var5(&Func::Mult, &func);
+	std::cout << "Delegate for member function: " << var5(-33) << std::endl;
+	var5 = func;
+	std::cout << "Delegate for class overloaded operator(): " << var5(-77) << std::endl;
+
+	Delegate<size_t(const char*)> var6(hasher);
+	std::cout << "Delegate for class with overloaded operator(): " << var6("fff") << std::endl;
+	auto var7(var6);
+	std::cout << "Delegate initialized from previous: " << var7("fff") << std::endl;
+
+	var2 = var5;
+	std::cout << "Delegate copied from another with the same type: " << var2(89) << std::endl;
+	var2 = &Func::Sqr;
+	std::cout << "Delegate for static function of the class: " << var2(111) << std::endl;
+
+	return 0;
+}
